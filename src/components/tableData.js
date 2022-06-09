058719af78aabf04b19033d40372cc7126a0c606
Mike Alexeenko <mikeA@ilmakiage.com>
2022-06-09 12:49:03 +0200

    TableData > finish add topic form, controlled components

--- src/components/tableData.js
+++ src/components/tableData.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// todo: move into components
 class TableData extends React.Component {
 
     constructor(props) {
@@ -8,6 +9,8 @@ class TableData extends React.Component {
         this.state = {
             topics: ['JSX', 'Rendering Elements', 'Components & Props', 'State & Lifecycle'],
             removedItem: null,
+            topic: '',
+            email: ''
         }
     }
 
@@ -17,13 +20,9 @@ class TableData extends React.Component {
      */
     handleRemoveRowClick = (item) => {
         if (item !== this.state.removedItem) {
-
             const result = [...this.state.topics].filter(word => word !== item);
-
-
             this.setState({
-                topics: result,
-                removedItem: item
+                topics: result, removedItem: item
             });
         }
     }
@@ -34,14 +33,28 @@ class TableData extends React.Component {
      */
     handleRestoreRowClick = (item) => {
         this.setState({
-            topics: [...this.state.topics, item],
-            removedItem: null
+            topics: [...this.state.topics, item], removedItem: null
         })
     }
 
+    /**
+     * Form submit
+     * @param event
+     */
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log(event.target[0].value)
+        this.setState({
+            topics: [...this.state.topics, this.state.topic],
+            topic: ''
+        })
+    }
+
+    handleInputChange = event => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
@@ -57,7 +70,6 @@ class TableData extends React.Component {
             </tr>;
         });
 
-
         let removedItem = this.state.removedItem;
 
         let message;
@@ -72,32 +84,46 @@ class TableData extends React.Component {
             </div>;
         }
 
-
-        return (
-            <div className="table-wrap">
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Topic</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {rows}
-                    </tbody>
-                </table>
-                {message}
-                <form className="form" >
-                    <label htmlFor="">
-                        <input className="form-control" type="text" placeholder='Add topic' name='topic'/>
-                    </label>
-                    <span className="btn btn-success" onClick={this.handleSubmit}>
-                        add item
-                    </span>
-                </form>
-            </div>
-        )
+        return (<div className="table-wrap">
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Topic</th>
+                    <th scope="col">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {rows}
+                </tbody>
+            </table>
+            {message}
+            <form className="form" onSubmit={this.handleSubmit}>
+                <label htmlFor="">
+                    <input
+                        className="form-control"
+                        type="text"
+                        placeholder='Add topic'
+                        name='topic'
+                        value={this.state.topic}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <label htmlFor="">
+                    <input
+                        className="form-control"
+                        type="email"
+                        placeholder='Email'
+                        name='email'
+                        value={this.state.email}
+                        onChange={this.handleInputChange}
+                    />
+                </label>
+                <button type='submit' className="btn btn-success">
+                    add item
+                </button>
+            </form>
+        </div>)
     }
 }
 
