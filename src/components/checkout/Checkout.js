53ed57f2402aeeb8a4fd181210ec5666ca7c36fe
Mike Alexeenko <mikeA@ilmakiage.com>
2022-06-30 11:00:38 +0200

    Checkout > code refactor

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -2,36 +2,23 @@ import React from "react";
 import CheckoutForm from "./checkoutForm";
 import TabsWidget from "../tabs/tabsWidget";
 
-function Checkout(props) {
+export default function Checkout(props) {
 
     let tabsConfig = {
         products: {
             title: 'Products',
             content: props.children
         },
-        customer:
-            {
-                title: 'Customer details',
-                content: <CheckoutForm/>
-            }
+        customer: {
+            title: 'Customer details',
+            content: <CheckoutForm/>
+        }
     }
 
     return (
         <div className="checkout-wrap">
             <h1>Checkout</h1>
-
             <TabsWidget tabs={tabsConfig}/>
-
-            {/*<div className="row">
-                <div className="col-md-6">
-                    {props.children}
-                </div>
-                <div className="col-md-6">
-                    <CheckoutForm />
-                </div>
-            </div>*/}
         </div>
     )
 }
-
-export default Checkout;
